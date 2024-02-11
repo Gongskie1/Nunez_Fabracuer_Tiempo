0c0fd47d4660ff7df363b1b342413d3bf229eddf
mark <markjosephtiempo1@gmail.com>
2024-02-11 20:05:30 +0800

    Can Create User

--- server/src/user_register/controller/user_register.js
+++ server/src/user_register/controller/user_register.js
@@ -9,9 +9,42 @@ module.exports =  {
             email
         } = req.body;
 
+        let create_user = {
+            username: username,
+            password: password,
+            email: email
+        }
         
+        let find_user;
+
+        try{
+            find_user = await User.findOne({email:email})
+        }catch (error){
+
+            return error
+        }
         
 
+        if(find_user !== null){
+            
+            return res.status(200).json(
+                {
+                    success:true,
+                    message: "A user existed.",
+                    data: null
+                }
+            )
+        }else{
+            const create = await User.create(create_user);
+
+            return res.status(201).json({
+                success:true,
+                message: "A user has been created.",
+                data: create
+            })
+        }
+
+        
         
     },
 
